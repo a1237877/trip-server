b9c857aa158832c43f446b9a6966a7c563231f12
a1237877 <487333601@qq.com>
2019-07-30 14:31:19 +0800

    !

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-//引入资源文件
-var createError = require('http-errors') //中间件
+// 引入资源文件
+var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var looger = require('morgan')
 
-//引入index.js 路由配置文件
+// 引入index.js路由配置文件
 var indexRouter = require('./routes/index')
-//引入user.js路由配置文件
+// 引入user.js路由配置文件
 var userRouter = require('./routes/user.js')
 
-var app = express()  //用express创建一个app
+var app = express() // 用express创建一个应用
 
-app.set('views',path.join(__dirname,'views')) //指定视图文件夹
-app.set('view engine','pug')  //指定视图引擎
+app.set('views', path.join(__dirname, 'views')) //指定视图文件夹views/
+app.set('view engine', 'pug')//指定视图引擎pug
 
-//使用刚刚加载的资源
+//使用刚加载的资源
 app.use(looger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended: false}))
 app.use(cookieParser()) //使用cookie
-app.use(express.static(path.join(__dirname,'public'))) //指定公共资源文件夹为public
-app.use('/',indexRouter)  //当路径为/ 也就是 'http://localhost:3000' 匹配index.js
-app.use('/user',userRouter) //当路径为/user 匹配 user.js
+app.use(express.static(path.join(__dirname, 'public'))) //指定公共资源文件夹为pubilc
 
-//匹配404 即路由未匹配上时
-app.use(function(req,res,next){
+app.use('/', indexRouter) //当路径为‘/’，也就是'http://localhost:3000'，匹配index.js
+app.use('/user', userRouter) //当路径为‘/user’，也就是'http://localhost:3000/user'，匹配user.js
+
+//匹配404，即路由未匹配时
+app.use(function(req, res, next) {
   next(createError(404))
 })
 
-//当路径匹配错误时
-app.use(function(err,req,res,next){
+// 当路径匹配错误时
+app.use(function(err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
